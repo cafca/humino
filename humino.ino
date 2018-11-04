c89e37f24453489b6a5afb76c1c6cdfa5b92c3b2
Vincent Ahrend <mail@vincentahrend.com>
2018-11-04 22:12:31 +0100

    Adapt sketch for capacitive sensors on Mega board

--- humino.ino
+++ humino.ino
@@ -1,38 +1,34 @@
 #include <SPI.h>
 
-const int POWER = 3;
 const int MEASUREMENT_DELAY = 5 * 60 * 1000;
-int output_value;
-String dataString;
+const int NUM_SENSORS = 16;
+
+int measurement;
+String outputString;
 
 void setup() {
-  pinMode(POWER, OUTPUT);
-  digitalWrite(POWER, LOW);
+
   Serial.begin(9600);
   while (!Serial) ; // wait for serial
-  delay(2000);
+  delay(500);
   Serial.println("status setup complete");
 }
 
 void loop() {
   Serial.println("status measuring... ");
-  // Switch transistor  
-  digitalWrite(POWER, HIGH);
-  delay(5);
-  // dataString will contain each measurement in CSV format
-  dataString = "";
-  for (int sensorPin = A0; sensorPin <= A5; sensorPin++) {
-    output_value = analogRead(sensorPin);
-    if (output_value > 999) {
-      output_value = 999;
+
+  outputString = "";
+  for (int sensorPin = 0; sensorPin < NUM_SENSORS; sensorPin++) {
+    measurement = analogRead(sensorPin);
+    if (measurement > 999) {
+      measurement = 999;
     }
-    dataString += output_value;
-    if (sensorPin != A5) {
-      dataString += ',';
+    outputString += measurement;
+    if (sensorPin != (NUM_SENSORS - 1)) {
+      outputString += ',';
     }
   }
-  digitalWrite(POWER, LOW);
   Serial.print("measurement ");
-  Serial.println(dataString);
+  Serial.println(outputString);
   delay(MEASUREMENT_DELAY);
 }
